d6d43e29e709f4c8d349aab4d5ceaecc22c189f5
Bryzz <alarangaerlan@gmail.com>
2016-02-03 12:07:24 +0100

    [UPDATE] Sécurité formulaire étudiant

--- vue/vueInscriptionEtu.php
+++ vue/vueInscriptionEtu.php
@@ -30,41 +30,131 @@ public function afficherFormulaireEtu(){
 		</p>
 		<br/>
 		<p>
+			<!--Les scripts pour vérifier chaque case-->
+      <script>
+      //On surligne les cases non valides
+      function surligne(champ, erreur) {
+      if(erreur)
+        champ.style.backgroundColor = "#fba";
+      else
+        champ.style.backgroundColor = "";
+      }
+
+      function verifString(champ, txt, longMax) {
+        if(champ.value.length > longMax) {
+          surligne(champ, true);
+          document.getElementById(txt).innerHTML = longMax + " caractères maximum autorisé";
+          return true;
+        } else {
+          surligne(champ, false);
+          document.getElementById(txt).innerHTML = "";
+          return false;
+        }
+      }
+
+      function verifTelephone(champ, txt) {
+        if(champ.value.length != 10 || !/^\d+$/.test(champ.value)) {
+          surligne(champ, true);
+          document.getElementById(txt).innerHTML = "Format invalide";
+          return true;
+        } else {
+          surligne(champ, false);
+          document.getElementById(txt).innerHTML = "";
+          return false;
+        }
+      }
+
+
+      function verifEmail(champ, txt){
+        var reg = new RegExp('^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+        if(!reg.test(champ.value)) {
+          surligne(champ, true);
+          document.getElementById(txt).innerHTML = "L'e-mail n'est pas valide.";
+          return true;
+        } else {
+          surligne(champ, false);
+          document.getElementById(txt).innerHTML = "";
+          return false;
+        }
+      }
+
+      function verifMdp(txt){
+        var passw = document.getElementById("passw");
+				var passwBis = document.getElementById("passwBis");
+        if (passw.value != passwBis.value) {
+          surligne(passw, true);
+          surligne(passwBis, true);
+          document.getElementById(txt).innerHTML = "Les 2 valeurs sont différentes";
+          return true;
+        } else if (passw.value.length > 20 || passw.value.length < 5) {
+          surligne(passw, true);
+          surligne(passwBis, true);
+          document.getElementById(txt).innerHTML = "Le mot de passe doit faire 5 à 20 caractères";
+          return true;
+        } else {
+          surligne(passw, false);
+          surligne(passwBis, false);
+          document.getElementById(txt).innerHTML = "";
+          return false;
+        }
+      }
+      </script>
+
+
 			<script type="text/javascript">
-				function VerifSubmit()	
+				EnableSubmit = function(val)
+				{
+				    var sbmt = document.getElementById("submit");
+
+				    if (val.checked == true)
+				    {
+				        sbmt.disabled = false;
+				    }
+				    else
+				    {
+				        sbmt.disabled = true;
+				    }
+				}
+			</script>
+			<script>
+			VerifSubmit = function()
 				{
 				html = html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 				var passw = document.getElementById("passw");
 				var passwBis = document.getElementById("passwBis");
-					if (/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(document.getElementById("mail").value))
-					  {  
-					    return true;
-					  } 
-					  else {
-					  	alert("L\'adresse email n'est pas correcte !")  ;
-					 	return false;
-					  }  	
-					 if (passw.value != passwBis.value) {
+					if (passw.value != passwBis.value) {
 							alert('Les mots de passe ne coïncident pas.');
 					        return false;
 					}
+					if (/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(document.getElementById("mail").value))
+					  {
+					    return true;
+					  }
+					  else {
+					  	alert("L\'adresse email n'est pas correcte !")  ;
+					 	return false;
+					  }
 				}
 			</script>
+
 			<form action="index.php" method="post" onSubmit="return VerifSubmit();">
 				<!-- Prenom-->
 				<label for="prenom"/> Prénom <span name="obligatoire">*</span>
 				<br/>
-				<input type="text" name="prenom" required/>
+				<input type="text" name="prenom" onblur="verifString(this,'messagePrenomContact','20')" required/>
+				<p id="messagePrenomContact" style="color:red"></p>
 				<br/><br/>
 				<!-- Nom -->
 				<label for="nom"/> Nom <span name="obligatoire">*</span>
 				<br/>
-				<input type="text" name="nom" required/>
+				<input type="text" name="nom" onblur="verifString(this,'messageNomContact','20')" required/>
+				<p id="messageNomContact" style="color:red"></p>
 				<br/><br/>
 				<!-- Telephone -->
 				<label for="tel"/> Téléphone (portable de préférence, sans espace. Ex : 0610203040) <span name="obligatoire">*</span>
+				<p id="messageTel" style="color:red"></p>
 				<br/>
-				<input type="text" name="tel" required/>
+				<input type="text" name="tel" onblur="verifTelephone(this, 'messageTel')" required/>
 				<br/><br/>
 				<!-- Engagement -->
 				<label for="engagement"/> Obligatoire <span name="obligatoire">*</span>
@@ -98,7 +188,8 @@ public function afficherFormulaireEtu(){
 				<br/>
 				<span name="detail">Elle servira à l'envoi d'informations relatives aux entretiens et à l'authentification</span>
 				<br/>
-				<input type="text" name="email" id="mail" required/>
+				<input type="text" name="email" id="mail" onblur="verifEmail(this, 'messageEmail')" required/>
+				<p id="messageEmail" style="color:red"></p>
 				<br/><br/>
 				<!--  Mdp -->
 				<label for="password"/> Mot de passe (il sera utilité pour l'authentification sur le site)<span name="obligatoire">*</span>
@@ -108,7 +199,8 @@ public function afficherFormulaireEtu(){
 				<!--  Mdp bis-->
 				<label for="passwordBis"/> Veuillez réécrire le mot de passe<span name="obligatoire">*</span>
 				<br/>
-				<input type="password" name="passwordBis" id="passwBis" equired/>
+				<input type="password" name="passwordBis" id="passwBis" onblur="verifMdp('messageMdp')" equired/>
+				<p id="messageMdp" style="color:red"></p>
 				<br/><br/>
 				<input type="hidden" name="inscription" value="etudiant"/>
 				<input type="submit" name="valid_inscription_etu"/>
