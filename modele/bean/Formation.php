c65f79e6bf4423ec59108ee0655ab869e097dee9
Bryzz <E145215S@U-INF-J-F113-15.iut-nantes.univ-nantes.prive>
2016-02-10 11:49:02 +0100

    [update] fix

---
+++ modele/bean/Formation.php
@@ -0,0 +1,19 @@
+<?php
+
+class ListeFormation {
+	private $departement;
+	private $initiales;
+	private $description;
+
+	public function getDepartement() {
+		return $this->departement;
+	}
+	public function getInitiales() {
+		return $this->initiales;
+	}
+	public function getDescription() {
+		return $this->description;
+	}
+}
+
+?>
